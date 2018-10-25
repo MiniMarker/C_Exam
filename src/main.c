eb11505b29c621923b02696be61123f8629efaba
MiniMarker <cfmarker@gmail.com>
2018-10-25 23:31:19 +0200

    search for line in file works

--- src/main.c
+++ src/main.c
@@ -1,37 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <pthread.h>
 #include <crypt.h>
 
-int readFile(char input[40]){
-    char password[20], *filePath;
+
+void* readFile(char inputHash[40]){
+    char password[20], line[20], *filePath;
+    FILE *dictionary_file;
 
     filePath = calloc(40, sizeof(char));
     
-    snprintf(filePath, 40, "./resources/dictionary.txt");
-
-    printf("%s\n", input);
-
-    FILE *dictionary_file;
+    snprintf(filePath, 31, "./src/resources/dictionary.txt");
+    
     dictionary_file = fopen(filePath, "r");
+    assert(dictionary_file != NULL);
+    /*
+    // TODO why doesnit it match?!
+    while (fgets(line, sizeof(line), dictionary_file) != 0) { 
 
-    for(int i = 0; i < 100; i++) {
+        printf("%s", line);
+
+        if(strcmp(inputHash, line) == 0) {
+            printf("FOUND: %s", line);
+            break;
+        }
+    }
+    */
+    
+    for(int i = 0; i < 10000; i++) {
 
         fscanf(dictionary_file, "%s", password);
-        printf("%s\n", password);
-
+        //printf("%d: %s\n", i, password);
+        
+        if(strcmp(inputHash, password) == 0){
+            printf("%d: %s\n", i, password);
+            break;
+        }
+        
     }
+
 
     fclose(dictionary_file);
     free(filePath);
-    return 0;
+    return NULL;
 };
-
-
 
 
 int main(int argc, char const *argv[]) {
 
-    readFile("test");
+    readFile("oi");
 
     return 0;
 }
