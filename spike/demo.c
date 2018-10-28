343fc11fb9d8572fe97ae947ae09806efc2d3514
MiniMarker <cfmarker@gmail.com>
2018-10-28 11:58:40 +0100

    working on thread and recursive spikes

--- spike/demo.c
+++ spike/demo.c
@@ -1,7 +1,2 @@
 #include "./include/demo.h"
 
-int sum(int a, int b){
-
-    return (a + b);
-
-};
---
+++ spike/recursive.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char* recursive(char *possibleChars, char* searchString, int index, int maxChar){
+	
+
+	return 0;
+
+};
+
+int main(int argc, char const *argv[]) {
+	
+	char *possibleChars = "abc";
+	int maxChar = 3;
+	char *searchString = "b";
+
+	recursive(possibleChars, searchString, 0, maxChar);
+
+	return 0;
+};
